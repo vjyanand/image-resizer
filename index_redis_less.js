1defb79def296da94acf00bb360ee5627f57d7f2
Vijay Anand <iavian@Vijays-MacBook-Pro.local>
2020-08-11 09:59:33 +0530

    Initial Commit

--- index_redis_less.js
+++ index_redis_less.js
@@ -6,6 +6,27 @@ const port = process.env.PORT || 8080
 const helmet = require('helmet');
 const fetch = require('node-fetch');
 const sharp = require('sharp');
+const { Pool } = require('pg')
+
+const pg = {
+    user: 'news',
+    host: 'colo.iavian.net',
+    database: 'news',
+    name: 'news',
+    password: '@xwDf!2d9Jy_bEN'
+}
+
+const pool = new Pool({
+    user: pg.user,
+    host: pg.host,
+    database: pg.database,
+    password: pg.password,
+    port: 5432,
+    connectionTimeoutMillis: 10000,
+    application_name: pg.name
+})
+
+
 router.get('/img', async function (req, res, next) {
     const url = decodeURI(req.query.url);
     if (!url) {
@@ -14,7 +35,7 @@ router.get('/img', async function (req, res, next) {
     if (!url.startsWith("http")) {
         return res.sendStatus(500)
     }
-    
+
     let width = parseInt(req.query.w, 10);
     let height = parseInt(req.query.h, 10);
     height = isNaN(height) ? null : height;
@@ -44,6 +65,38 @@ router.get('/img', async function (req, res, next) {
         }).catch(err => {
             return res.status(500);
         })
+});
+
+router.get('/feed', async function (req, res, next) {
+    let filterIds = [20545835, 10774652, 457984599, 28614262, 19211550, 1367531, 14669951, 8007074, 1917731, 18856867, 39308549]
+    const client = await pool.connect()
+    let feeds = []
+    try {
+        let result = await client.query("SELECT row_number() OVER (PARTITION BY true) as id, url, img, title, date_part('epoch', ts)::int as ts, COALESCE(imgdimension, 1) as dim, source FROM feedview WHERE source = ANY($1::INT[]) ORDER BY ts DESC", [feedIds])
+        feeds = result.rows
+      } catch (err) {
+        console.log(err)
+      } finally {
+        client.release();
+      }
+    let uniqueUrls = []
+    let uniqueFeeds = feeds.reduce(function (acc, feed) {
+        if (uniqueUrls.includes(feed.url)) {
+            return acc;
+        }
+        if (!feed.img) {
+            return acc;
+        }
+        uniqueUrls.push(feed.url)
+        acc.push(feed)
+        return acc;
+    }, []);
+    let result = {
+        feed: uniqueFeeds,
+        h: 'https://drudgenow.com/article/'
+    }
+    res.charset = "utf-8";
+    return res.status(200).json(result);
 });
 
 app.use(helmet())
