cb645f1c5a069942684c8389fe66e3f9b0279b97
vjt <reg@iavian.com>
2021-02-04 18:11:53 +0530

    ROLL BACK PGSQL

--- index_redis_less.js
+++ index_redis_less.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8080
 const helmet = require('helmet');
 const fetch = require('node-fetch');
 const sharp = require('sharp');
-const HttpsProxyAgent = require('https-proxy-agent');
+const HttpsProxyAgent = require('https-proxy-agent')
 
 const {
     Pool
@@ -69,7 +69,7 @@ router.get('/img', async function (req, res, next) {
                 agent:new HttpsProxyAgent('http://proxy.iavian.net:38080'),
                 headers: {
                     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15",
-                    compress: true,
+                    compress: false,
                 }
             });
             if (!fetchResponse.ok) {
