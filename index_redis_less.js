92c018ea6b36d62c05f3568091690c1f7a142047
vjy <vijay@iavian.com>
2022-01-22 11:39:03 +0530

    old version

--- index_redis_less.js
+++ index_redis_less.js
@@ -7,29 +7,7 @@ const helmet = require('helmet');
 const Fetch = require('@adobe/helix-fetch');
 const fetch = Fetch.fetch;
 const sharp = require('sharp');
-const {
-    Pool
-} = require('pg');
 const morgan = require('morgan')
-const fs = require('fs')
-
-const pg = {
-    user: 'news',
-    host: 'colo.iavian.net',
-    database: 'news',
-    name: 'news',
-    password: '@xwDf!2d9Jy_bEN'
-}
-
-const pool = new Pool({
-    user: pg.user,
-    host: pg.host,
-    database: pg.database,
-    password: pg.password,
-    port: 5432,
-    connectionTimeoutMillis: 10000,
-    application_name: pg.name
-})
 
 router.get('/img', async function (req, res, next) {
     const url = decodeURI(req.query.url);
@@ -139,39 +117,6 @@ router.get('/favicon', async function (req, res, next) {
         }
     }
     return res.sendStatus(500)
-});
-
-router.get('/feed', async function (req, res, next) {
-    let filterIds = [20545835, 10774652, 457984599, 28614262, 19211550, 1367531, 14669951, 8007074, 1917731, 18856867, 39308549]
-    const client = await pool.connect()
-    let feeds = []
-    try {
-        let result = await client.query("SELECT row_number() OVER (PARTITION BY true) as id, url, img, title, date_part('epoch', ts)::int as ts, COALESCE(imgdimension, 1) as dim, source FROM feedview WHERE source = ANY($1::INT[]) ORDER BY ts DESC", [filterIds])
-        feeds = result.rows
-    } catch (err) {
-        console.log(err)
-    } finally {
-        client.release();
-    }
-    let uniqueUrls = []
-    let uniqueFeeds = feeds.reduce(function (acc, feed) {
-        if (uniqueUrls.includes(feed.url)) {
-            return acc;
-        }
-        if (!feed.img) {
-            return acc;
-        }
-        uniqueUrls.push(feed.url)
-        acc.push(feed)
-        return acc;
-    }, []);
-    let result = {
-        feed: uniqueFeeds,
-        h: 'https://drudgenow.com/article/'
-    }
-    res.charset = "utf-8";
-    res.header('Cache-Control', 'public, max-age=300, immutable')
-    return res.status(200).json(result);
 });
 
 app.use(helmet())
