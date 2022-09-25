dcd87bc439f3e181f494d1755a640f45da855544
Vijay Anand <iavian@Vijays-MacBook-Pro.local>
2022-09-25 16:55:45 +0530

    a

--- index.js
+++ index.js
@@ -20,9 +20,12 @@ router.get('/proxy', async function (req, res, next) {
     let fetchResponse = await fetch(url, {
         timeout: 5000,
         headers: {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15",
             "Accept-Language": "en-us",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            'Referer': 'https://www.google.com/',
+            'DNT': '1',
+            'upgrade-insecure-requests': '1',
         },
         compress: true,
     });
